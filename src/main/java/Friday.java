522cb3296bbb386939d7b55a066b8e68abe95f32
KarKuen <karkuenkakkoii@gmail.com>
2025-01-23 20:04:45 +0800

    Friday now stores the text inputs and display them back to the user when requested.

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,10 +1,13 @@
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Friday {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
+
+        ArrayList l = new ArrayList<String>();
 
         System.out.println(
                 "____________________________________\n" +
@@ -14,7 +17,7 @@ public class Friday {
         );
 
         while (true) {
-            String logo = in.next();
+            String logo = in.nextLine();
             if (logo.compareTo("bye") == 0) {
                 System.out.println(
                         "____________________________________\n" +
@@ -22,12 +25,23 @@ public class Friday {
                                 "\n____________________________________\n"
                 );
                 break;
+            } else if (logo.compareTo("list") == 0) {
+                int counter = 1;
+                System.out.println("____________________________________\n");
+                for (int i=0; i< l.size(); i++) {
+                    System.out.println(counter + ". " + l.get(i));
+                    counter ++;
+                }
+                System.out.println("____________________________________\n");
+
+            } else {
+                System.out.println(
+                        "____________________________________\n" +
+                                "added: " + logo +
+                                "\n____________________________________\n"
+                );
+                l.add(logo);
             }
-            System.out.println(
-                    "____________________________________\n" +
-                    logo +
-                    "\n____________________________________\n"
-            );
         }
         out.close();
     }
