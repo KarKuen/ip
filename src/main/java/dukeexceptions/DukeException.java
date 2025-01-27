8e7c8fd799e116bee6b833a8f373ec1ff1d48b85
KarKuen <karkuenkakkoii@gmail.com>
2025-01-27 12:15:07 +0800

    Add LocalDateTime to verify the format of the DeadlineTask date

--- src/main/java/dukeexceptions/DukeException.java
+++ src/main/java/dukeexceptions/DukeException.java
@@ -1,7 +1,7 @@
 package dukeexceptions;
 
 public class DukeException extends Exception {
-    public DukeException() {
-        super();
+    public DukeException(String message) {
+        super(message);
     }
 }
